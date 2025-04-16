85a2b254c7b4f6d969ce7581b7e73077fee23f2a
Mostafa rayan <mr3309296@gmail.com>
2025-04-16 15:03:01 +0200

    add to cart btn to all products box

--- src/_components/sharable/ProjectBox.tsx
+++ src/_components/sharable/ProjectBox.tsx
@@ -1,3 +1,4 @@
+import AddToCart from "@/app/shop/[id]/AddToCart";
 import { Project } from "@/types/type";
 import { Captions, CircleDollarSign } from "lucide-react";
 import Image from "next/image";
@@ -5,24 +6,30 @@ import Link from "next/link";
 
 const ProjectBox = ({ item }: { item: Project }) => {
   return (
-    <Link className="space-y-3" href={`/shop/${item._id}`} key={item._id}>
-      <div className="w-full  h-[250px] relative">
-        <Image
-          className="rounded-lg"
-          src={item.images[0]}
-          alt={item.title}
-          fill
-        />
+    <>
+      <Link className="space-y-3" href={`/shop/${item._id}`} key={item._id}>
+        <div className="w-full  h-[250px] relative">
+          <Image
+            className="rounded-lg"
+            src={item.images[0]}
+            alt={item.title}
+            fill
+          />
+        </div>
+        <h2 className="flex items-center gap-2 ">
+          <Captions className="size-6 text-primary" />
+          <span className="line-clamp-1">{item.title}</span>
+        </h2>
+        <h2 className="text-muted-foreground flex items-center gap-2 ">
+          <CircleDollarSign className="size-6 text-primary" />
+          {item.price}$
+        </h2>
+      </Link>
+
+      <div className="mt-3">
+        <AddToCart data={item} />
       </div>
-      <h2 className="flex items-center gap-2 ">
-        <Captions className="size-6 text-primary" />
-        <span className="line-clamp-1">{item.title}</span>
-      </h2>
-      <h2 className="text-muted-foreground flex items-center gap-2 ">
-        <CircleDollarSign className="size-6 text-primary" />
-        {item.price}$
-      </h2>
-    </Link>
+    </>
   );
 };
 
